04c976521866dc2778d71dc995923cd9e6975cd5
hiennv6654 <hiennv6654@co-well.com.vn>
2016-11-28 10:07:10 +0700

    Commit Code

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wavefront OBJ Loader
+//
+//  Created by Jeff LaMarche on 12/14/08.
+//  Copyright Jeff LaMarche 2008. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "Wavefront_OBJ_LoaderAppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Wavefront_OBJ_LoaderAppDelegate class]));
+    }
+}
